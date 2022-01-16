7851a9ebd019c7aa57fbfa53b08645325ceb0695
CamilleAbella <camille.abella@hotmail.fr>
2022-01-16 19:17:53 +0100

    fix ignoredRole table

--- src/tables/ignoredRole.ts
+++ src/tables/ignoredRole.ts
@@ -1,13 +1,15 @@
 import * as app from "../app.js"
 
 export interface IgnoredRole {
-  // type of table
+  role_id: string
+  guild_id: string
 }
 
 export default new app.Table<IgnoredRole>({
   name: "ignoredRole",
   description: "The ignoredRole table",
   setup: (table) => {
-    // setup table columns => http://knexjs.org/#Schema-Building
+    table.string("role_id").notNullable().unique()
+    table.string("guild_id").notNullable()
   },
 })
